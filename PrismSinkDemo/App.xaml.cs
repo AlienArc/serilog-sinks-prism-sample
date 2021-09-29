0102d160d71f9cecce1a77b6236cf9034b3f6eb1
Duane Newman <duane@alienarc.com>
2021-09-29 11:14:26 -0500

    initial sample

---
+++ PrismSinkDemo/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Serilog.Sinks.Prism;
+using PrismSinkDemo.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Serilog;
+using System.IO;
+using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Prism.Events;
+using Serilog.Formatting.Display;
+
+namespace PrismSinkDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            var eventAggregator = Container.Resolve<IEventAggregator>();
+
+            var configuration = new ConfigurationBuilder()
+				 .SetBasePath(Directory.GetCurrentDirectory())
+				 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+				 .Build();
+
+			Log.Logger = new LoggerConfiguration()
+				.ReadFrom.Configuration(configuration)
+                .WriteTo.EventAggregator(eventAggregator, "{Message: lj}")
+                .CreateLogger();
+
+            containerRegistry.RegisterInstance(Log.Logger);
+
+        }
+    }
+}
---
+++ PrismSinkDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Prism.Commands;
+using Prism.Events;
+using Prism.Mvvm;
+using Serilog;
+using Serilog.Sinks.Prism;
+using System.Windows.Input;
+
+namespace PrismSinkDemo.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private string _message = string.Empty;
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+        public ICommand DoItCommand { get; }
+        public ILogger Logger { get; }
+        public IEventAggregator EventAggregator { get; }
+
+        private int number = 0;
+        public MainWindowViewModel(ILogger logger, IEventAggregator eventAggregator)
+        {
+            Logger = logger;
+            EventAggregator = eventAggregator;
+            EventAggregator.GetEvent<EventAggregatorSinkEvent>().Subscribe(OnLogEvent);
+            DoItCommand = new DelegateCommand(() => { Title = $"The Number is now {++number}"; Logger.Fatal(Title); });
+        }
+
+        private void OnLogEvent(string message)
+        {
+            Message = message;
+        }
+    }
+}
